d0c3471c218dc8a2bb23c09f77a345a95037f02f
lotziko <vasya.tovt.98@gmail.com>
2023-09-22 15:11:02 +0300

    Removed unused files/changed shader extension

--- BlueberryEngine/src/Blueberry/Content/ShaderImporter.cpp
+++ BlueberryEngine/src/Blueberry/Content/ShaderImporter.cpp
@@ -7,7 +7,7 @@ namespace Blueberry
 {
 	Ref<Object> ShaderImporter::Import(const std::string& path)
 	{
-		std::wstring shaderPath = std::wstring(path.begin(), path.end()).append(L".hlsl");
+		std::wstring shaderPath = std::wstring(path.begin(), path.end()).append(L".shader");
 		return Shader::Create(shaderPath);
 	}
 
